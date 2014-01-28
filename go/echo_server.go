6a23be2c96f576eaecdb09ff25934adfae1400da
bob hancock <rhancock@appnexus.com>
2014-01-27 19:45:40 -0500

    restored echo_server.go

--- go/echo_server.go
+++ go/echo_server.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-    "bufio"
-    "log"
-    "net"
-	"strconv"
-	"os"
+	"bufio"
 	"fmt"
+	"log"
+	"net"
+	"os"
 	"runtime"
+	"strconv"
 	//"time"
 	//"stats"
 	"strings"
@@ -32,46 +32,46 @@ func main() {
 	}
 	runtime.GOMAXPROCS(max_procs)
 
-    port := ":" + os.Args[2]
-    // ln, err := net.Listen("tcp", ":2020")
-    ln, err := net.Listen("tcp", port)
-    if err != nil {
-        log.Fatalf("listen error: %v", err)
-    }
-    accepted := 0
-    for {   
-        conn, err := ln.Accept()
-        if err != nil {
-            log.Fatalf("accept error: %v", err)
-        }
-        accepted++
-        go serve(conn)
+	port := ":" + os.Args[2]
+	// ln, err := net.Listen("tcp", ":2020")
+	ln, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("listen error: %v", err)
+	}
+	accepted := 0
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatalf("accept error: %v", err)
+		}
+		accepted++
+		go serve(conn)
 
 		/*for {
-		a := time.Seconds()
-		var ftimes []float64
-			for _, tv := range times {
-				//		fmt.Printf("%d %f\n", tv, float64(tv))
-				ftimes = append(ftimes, float64(tv))
-			}
-			fmt.Printf("Max: %f\n", stats.StatsMax(ftimes) * 1e-9)
-			fmt.Printf("Min: %f\n", stats.StatsMin(ftimes) * 1e-9)
-			fmt.Printf("Std: %f\n", stats.StatsSampleStandardDeviation(ftimes) * 1e-9)
-		}*/
-        //log.Printf("Accepted %d", accepted)
-    }
+						  a := time.Seconds()
+				          var ftimes []float64
+				          for _, tv := range times {
+				//                fmt.Printf("%d %f\n", tv, float64(tv))
+				                  ftimes = append(ftimes, float64(tv))
+		         }
+		         fmt.Printf("Max: %f\n", stats.StatsMax(ftimes) * 1e-9)
+		         fmt.Printf("Min: %f\n", stats.StatsMin(ftimes) * 1e-9)
+		         fmt.Printf("Std: %f\n", stats.StatsSampleStandardDeviation(ftimes) * 1e-9)
+						                                                                                                                                                                                                                              }*/
+		//log.Printf("Accepted %d", accepted)
+	}
 }
 
 func serve(conn net.Conn) {
-    bufr := bufio.NewReader(conn)
-	
-    for { 
+	bufr := bufio.NewReader(conn)
+
+	for {
 		//a := time.Nanoseconds()
 
-        line, err := bufr.ReadString('\n')
-        if err != nil {
-            return
-        }
+		line, err := bufr.ReadString('\n')
+		if err != nil {
+			return
+		}
 		if line == "" {
 			return
 		}
@@ -80,8 +80,8 @@ func serve(conn net.Conn) {
 			conn.Write([]byte("ACK\n"))
 		} else {
 			conn.Write([]byte(line))
-            fmt.Println(line)
+			fmt.Println(line)
 		}
-	//	times = append(times, time.Nanoseconds() - a)
-    }
+		//        times = append(times, time.Nanoseconds() - a)
+	}
 }
